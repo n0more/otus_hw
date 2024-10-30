d5243e3ecce07abdeae22b976865d9ae9e506806
Болотов Денис Викторович <dvbolotov@beeline.ru>
2024-10-30 20:28:00 +0300

    Домашнее задание hw03

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// Place your code here.
+	var size int
+	// var type bool
+
+	fmt.Printf("Ведите размер доски:")
+	fmt.Scanf("%d", &size)
+
+	fmt.Printf("Размер доски: %v на %v\n", size, size)
+	fmt.Println("")
+
+	for i := 0; i < size; i++ {
+		for u := 0; u < size; u++ {
+			if i%2 == u%2 {
+				fmt.Printf("#")
+			} else {
+				fmt.Printf("_")
+			}
+		}
+		fmt.Println("")
+	}
 }
